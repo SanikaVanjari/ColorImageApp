49d7b2abf8aba8ee4fe66b0cb6689d611e9bb014
Sanika Vanjari <sanikavanjari94@gmail.com>
2020-08-22 22:35:29 +0530

    CameraX dependencies added

--- app/src/main/java/com/example/imagecolorapp/MainActivity.kt
+++ app/src/main/java/com/example/imagecolorapp/MainActivity.kt
@@ -13,6 +13,7 @@ import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.view.Menu
+import androidx.navigation.NavController
 
 class MainActivity : AppCompatActivity() {
 
@@ -42,6 +43,8 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
